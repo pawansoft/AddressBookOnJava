d48a5c49afe9b9a4a07f88c6ac6a0810ac1a2658
Pawan Kumar <pk.soft29@gmail.com>
2020-11-04 14:59:34 +0530

    Added : The ability to create contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,17 @@
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook Project !!!!");
+        ContactDetails contactDetails = new ContactDetails();
+
+        //setting the value of contact details
+        contactDetails.setfName("Pawan");
+        contactDetails.setlName("Kumar");
+        contactDetails.setEmailId("pk.soft29@gmail.com");
+        contactDetails.setPhoneNumber("91 8083373213");
+        contactDetails.setAddress("Rampur");
+        contactDetails.setZip(804426);
+        contactDetails.setCity("Arwal");
+        contactDetails.setState("Bihar");
+
+        System.out.println("Details of Person" +contactDetails);
     }
 }
---
+++ ContactDetails.java
@@ -0,0 +1,94 @@
+public class ContactDetails {
+    //Declaring the contact detail variables
+    private String fName;
+    private String lName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private String phoneNumber;
+    private String emailId;
+
+    //generating getter setter method to add or populate the value form variable
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    //Overriding toString method
+
+    @Override
+    public String toString() {
+        return "ContactDetails{" +
+                "fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+}
